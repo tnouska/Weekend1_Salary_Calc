20da509180ac0930bfd9240b084276c5693c178a
Teagan Nouska <tnouska@live.com>
2018-03-10 19:26:54 -0600

    finished base mode

--- client.js
+++ client.js
@@ -1,10 +1,58 @@
-
+//Classes
+class Employee {
+  constructor(fNameIn, lNameIn, idNumIn, jobTitleIn, annualSalaryIn) {
+    this.fName = fNameIn;
+    this.lName = lNameIn;
+    this.idNum = idNumIn;
+    this.jobTitle = jobTitleIn;
+    this.annualSalary = annualSalaryIn;
+  }
+}
+//Global Variables
+let allWorkers = [];
 
 $(document).ready(readyNow);
 
 function readyNow() {
-
+eventHandler();
 }
+
 function eventHandler() {
-  $('#submitButton').on('click'); //need to add some function
+  $('#submitButton').on('click', addWorker);
+}
+//functions for eventHandler
+function createEmoployee() {
+  let fNameIn = $('#fnInput').val();
+  let lNameIn = $('#lnInput').val();
+  let idNumIn = $('#idInput').val();
+  let jobTitleIn = $('#titleInput').val();
+  let annualSalaryIn = $('#salaryInput').val();
+
+  let worker = new Employee (fNameIn, lNameIn, idNumIn, jobTitleIn, annualSalaryIn);
+  allWorkers.push(worker);
+}
+function calcTotalSalary() {
+  let totalSalary = 0;
+  for (var i = 0; i < allWorkers.length; i++) {
+    totalSalary += parseInt(allWorkers[i].annualSalary);
+  }
+  $('#totalSalary').empty();
+  if (totalSalary > 20000) {
+    $('#totalSalary').append('<h3 class="redSalary">Total Monthly Salary Cost: $' + totalSalary.toFixed(2) + '</h3>');
+  }
+  else if (totalSalary <= 20000) {
+    $('#totalSalary').append('<h3 class="greenSalary">Total Monthly Salary Cost: $' + totalSalary.toFixed(2) + '</h3>');
+  }
+}
+function addWorker() {
+createEmoployee();
+$('#tableBody').empty();
+for (let worker of allWorkers) {
+  $('#tableBody').append('<tr>' + '<td>' + worker.fName + '</td>' +
+                        '<td>' + worker.lName + '</td>' +
+                        '<td>' + worker.idNum + '</td>' +
+                        '<td>' + worker.jobTitle + '</td>' +
+                        '<td class="salary">' + worker.annualSalary + '</td>' + '</tr>');
+                      }
+calcTotalSalary();
 }
