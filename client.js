fd79702ad3e8288a59b5c5ccb78e785509352a77
Teagan Nouska <tnouska@live.com>
2018-03-11 20:49:53 -0500

    updated total salary to actually calculate monthly total instead of annual total

--- client.js
+++ client.js
@@ -8,19 +8,21 @@ class Employee {
     this.annualSalary = annualSalaryIn;
   }
 }
+
 //Global Variables
 let allWorkers = [];
 
+//run when document is ready.
 $(document).ready(readyNow);
 
+//readynow function.
 function readyNow() {
 eventHandler();
 }
-
+// eventHandler function.
 function eventHandler() {
   $('#submitButton').on('click', addWorker);
   $('#removeButton').on('click', removeEmployeeArray);
-  // $('#tableBody').on('click', '.workerSalary', removeEmployeeDom);
 }
 //functions for eventHandler
 function addWorker() {
@@ -28,46 +30,61 @@ createEmoployee();
 updateDom();
 }
 function createEmoployee() {
+  //inputs
   let fNameIn = $('#fnInput').val();
   let lNameIn = $('#lnInput').val();
   let idNumIn = $('#idInput').val();
   let jobTitleIn = $('#titleInput').val();
   let annualSalaryIn = $('#salaryInput').val();
+  //inputs into class constructor
+  if (fNameIn === "") {
+    $('#fnInput').prop('required', true);
+  } else if (lNameIn === "") {
+    $('#lnInput').prop('required', true);
+  } else if (idNumIn === "") {
+    $('#idNumIn').prop('required', true);
+  } else if (jobTitleIn === "") {
+    $('#titleInput').prop('required', true);
+  } else if (annualSalaryIn === "") {
+    $('#salaryInput').prop('required', true);
+  } else {
 
-  let worker = new Employee (fNameIn, lNameIn, idNumIn, jobTitleIn, annualSalaryIn);
-  allWorkers.push(worker);
+    let worker = new Employee (fNameIn, lNameIn, idNumIn, jobTitleIn, annualSalaryIn);
+    allWorkers.push(worker);
+  }
+
 }
 function calcTotalSalary() {
   let totalSalary = 0;
   for (var i = 0; i < allWorkers.length; i++) {
     totalSalary += parseInt(allWorkers[i].annualSalary);
-    console.log('allWorkers[i]',allWorkers[i].annualSalary);
-    console.log('totalSalary',totalSalary);
-  }
+  }//loop through array of allworkers and update total salary
   $('#totalSalary').empty();
-  if (totalSalary > 20000) {
-    $('#totalSalary').append('<h3 class="redSalary">Total Monthly Salary Cost: $' + totalSalary.toFixed(2) + '</h3>');
-  }
-  else if (totalSalary <= 20000) {
-    $('#totalSalary').append('<h3 class="greenSalary">Total Monthly Salary Cost: $' + totalSalary.toFixed(2) + '</h3>');
-  }
+  if ((totalSalary/12) > 20000) {
+    $('#totalSalary').append('<h3 class="redSalary">Total Monthly Salary Cost: $' + (totalSalary.toFixed(2) / 12) + '</h3>');
+  }// compare total salary and append to dom
+  else if ((totalSalary/12) <= 20000) {
+    $('#totalSalary').append('<h3 class="greenSalary">Total Monthly Salary Cost: $' + (totalSalary.toFixed(2) / 12) + '</h3>');
+  }// compare total salary and append to dom
 }
 function updateDom() {
   $('#tableBody').empty();
   for (let worker of allWorkers) {
-    $('#tableBody').append('<tr class="workerInfo">' + '<td>' + worker.fName + '</td>' +
-                          '<td>' + worker.lName + '</td>' +
-                          '<td id="empId">' + worker.idNum + '</td>' +
-                          '<td>' + worker.jobTitle + '</td>' +
-                          '<td class="workerSalary">' + worker.annualSalary + '</td>' + '</tr>');
-                        }
-  calcTotalSalary();
+    $('#tableBody').append('<tr class="worker Info">' + '<td class="worker data">' + worker.fName + '</td>' +
+                          '<td class="worker data">' + worker.lName + '</td>' +
+                          '<td class="worker data" id="empId">' + worker.idNum + '</td>' +
+                          '<td class="worker data">' + worker.jobTitle + '</td>' +
+                          '<td class="worker data">' + worker.annualSalary + '</td>' + '</tr>');
+                        }// loop through allworkers array and append each worker to dom.
+  calcTotalSalary();// update total salary
 }
 function removeEmployeeArray() {
   let inputEmployee = $('#removeInput').val();
-  for (var x = 0; x < allWorkers.length; x++) {
-    if (inputEmployee === allWorkers[x].idNum) {
-      allWorkers.splice(x, 1);
-    }
-  }updateDom();
+  for (var i = 0; i < allWorkers.length; i++) {
+    if (inputEmployee === allWorkers[i].idNum) {
+      allWorkers.splice(i, 1);
+    }// takes user input and matches empId to the worker in array and removes them
+    //from the array.
+  }updateDom();// this will then update the total salary and table with the people
+  //that are left over in the array.
 }
